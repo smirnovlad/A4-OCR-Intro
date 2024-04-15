f9e826f07b8103a0c76535af07047797f5ad4342
Smirnov Vlad <smirnovlad03@gmail.com>
2024-04-15 22:26:51 +0300

    task3 is initialized

---
+++ task3/utils/metrics.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.smooth = 1e-8
+
+    def forward(self, input, target):
+        num = 2 * torch.sum(torch.sigmoid(input) * target, axis=(1, 2, 3))
+        den = torch.sum(torch.sigmoid(input) + target, axis=(1, 2, 3))
+        res = 1 - (num + self.smooth) / (den + self.smooth)
+        return torch.mean(res)
+
+class CombinedLoss(nn.Module):
+    def __init__(self, alpha=0.5):
+        super().__init__()
+
+        self.bce = nn.BCEWithLogitsLoss()
+        self.dice_loss = DiceLoss()
+
+    def forward(self, pred, target):
+        return self.alpha * self.dice_loss(pred, target) + (1 - self.alpha) * self.bce(pred, target)
